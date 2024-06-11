c54b1375878fb4f0cc8195d117530ab105bccfcd
Brian Marick <marick@exampler.com>
2024-06-11 14:47:37 -0500

    make Lenses.Use

--- lib/lenses.ex
+++ lib/lenses.ex
@@ -6,11 +6,5 @@ defmodule Lens2.Lenses do
 
   alias Lens2.Lenses.{Basic, Indexed, Combine, Keyed}
   import Lens2.Helpers.Delegate
-  use Lens2.Compatible.OriginalLenses
-
-
-  delegate_to(Combine, [
-    levels_below(descender),
-    add_levels_below(descender),
-  ])
+  use Lens2.Lenses.Use
 end
