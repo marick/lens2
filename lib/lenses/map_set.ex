292505de63f6fd78fa292beb74f028aac516be31
Brian Marick <marick@exampler.com>
2024-07-17 16:07:28 -0500

    Tweak MapSet.all documentation and use doctests

--- lib/lenses/map_set.ex
+++ lib/lenses/map_set.ex
@@ -6,42 +6,30 @@ defmodule Lens2.Lenses.MapSet do
   use Lens2
 
   @doc """
-  Like `Lens.all/0`, but intended for use with a MapSet.
+  Like `Lens.all/0`, but intended for use with a `MapSet`.
 
-  `all` is pointless without other lenses appended to look
-  deeper into the nested structure.
+  Here's how to update the `:a` values in a `MapSet` of maps:
 
-  Consider this lens and this data:
-
-       lens =
-         Lens.MapSet.all |> Lens.key?(:a)
-       input =
-         MapSet.new([%{a: 1}, %{a: 2}, %{a: 3}, %{vvvv: "unchanged"}])
-
-  You can now increment the values of all the `:a` values like this:
-
-       Deeply.update(input, lens, & &1*100)
-       > MapSet.new([%{a: 100}, %{a: 200}, %{a: 300}, %{vvvv: "unchanged"}])
-
-  Note that the question mark in `Lens.key?` is required, else the
-  multiplication function will be called on `nil`.
+       iex> lens = Lens.MapSet.all |> Lens.key?(:a)
+       iex> container = MapSet.new([%{a: 1}, %{a: 2}, %{a: 3}])
+       iex> Deeply.update(container, lens, & &1*100)
+       MapSet.new([%{a: 100}, %{a: 200}, %{a: 300}])
 
   `all` also works reasonably well with `put`. Given the above
-  `input` mapset and `lens`,
+  container and lens,
 
-       Deeply.put(input, lens, 3)
+       Deeply.put(container, lens, 3)
 
   ... will produce:
 
-       MapSet.new([%{a: 3}, %{vvvv: "unchanged"}]
+       MapSet.new([%{a: 3}]
 
   Note that the multiple maps with now-identical `:a` values have
-  been correctly collapsed into one.
+  been correctly collapsed into one. (MapSets can't have duplicates.)
 
-  As is typical for `Lens`, `get` functions return lists rather than
-  the base type:
+  As normal, `get` functions return lists rather than MapSets:
 
-       Deeply.get_all(input, lens)
+       Deeply.get_all(container, lens)
        > [1, 2, 3]    # *not* MapSet.new([1, 2, 3])
 
   """
