8a8ee5d92e7e76273ea9dd4cbe335542d2c0771c
Brian Marick <marick@exampler.com>
2024-08-07 17:24:45 -0500

    Add Keyword lens makers

--- lib/lenses/keyword.ex
+++ lib/lenses/keyword.ex
@@ -2,34 +2,80 @@ alias Lens2.Lenses
 
 defmodule Lenses.Keyword do
   @moduledoc """
-  A variant of `Lens2.Lenses.Keyed` that applies to all matching `Keyword` keys.
+  Lenses that support duplicate keys in keyword lists.
 
-  The `Kernel` "_in" functions apply only to the *first* matching key in a `Keyword` list:
+  The lens makers in `Lens2.Lenses.Keyed` only operate on the first
+  matching key. These operate on all values, as in the distinction
+  between `Keyword.get/3` and `Keyword.get_values/2`.
 
-      iex(3)> keylist = [a: 1, other: 2, a: 3]
-      [a: 1, other: 2, a: 3]
-      iex(4)> get_in(keylist, [:a])
-      1
-      iex(5)> put_in(keylist, [:a], 33)
-      [a: 33, other: 2, a: 3]
-      iex(6)> update_in(keylist, [:a], & &1 + 100)
-      [a: 101, other: 2, a: 3]
+  The `Deeply.update` and `Deeply.put` functions produce keyword
+  lists. (Lenses from `Lens2.Lenses.Keyed` produce maps.)
 
-  Lenses like `Lens2.Lenses.Keyed.key/1` mimic that behavior. (Indeed,
-  they're implemented using it.) Given that the Joe Q. Average lens
-  points at multiple places, that's counterintuitive. The lenses in this
-  module will get, put, and update all matching keys.
-
-
-
-  Problems: duplicate keys.
-
-  Some set-type operations don't produce a keyword list.
-
-
-  Deeply.put([a: 1], Lens.map_values, :NEW)
-  %{NEW: 1}
   """
 
+  use Lens2
+  import Lens2.Helpers.Assert
 
+  @doc ~S"""
+  Returns a lens that points to all values of `key`.
+
+      iex> kw = [a: 1, b: 2, a: 3]
+      iex> lens = Lens.Keyword.key?(:a)
+      iex> Deeply.get_all(kw, lens)
+      [1, 3]
+      iex> Deeply.update(kw, lens, & &1 * 1111)
+      [a: 1111, b: 2, a: 3333]
+
+  Note that a missing key cannot be added:
+
+      iex> Deeply.put([a: 1, b: 2], Lens.Keyword.key?(:missing), :NEW)
+      [a: 1, b: 2]
+
+  If you want to add key/value pairs, consider lens makers like
+  `Lens2.Lenses.Indexed.front/0`:
+
+      iex> Deeply.put([a: 1, b: 2], Lens.front, {:missing, :NEW})
+      [missing: :NEW, a: 1, b: 2]
+  """
+
+  @spec key?(atom) :: Lens2.lens
+  defmaker key?(key) do
+    assert_atom(key)
+    Lens.all |> Lens.filter(fn {k, _v} -> k == key end) |> Lens.at(1)
+  end
+
+
+  @doc ~S"""
+  Returns a lens that points to the values of the given keys. Missing
+  keys are to be ignored.
+
+      iex> kw = [a: 1, b: 2, a: 3]
+      iex> lens = Lens.Keyword.keys?([:a, :missing])
+      iex> Deeply.get_all(kw, lens)
+      [1, 3]
+      iex> Deeply.update(kw, lens, & &1 * 1111)
+      [a: 1111, b: 2, a: 3333]
+
+  """
+  @spec keys?(list(atom)) :: Lens2.lens
+  defmaker keys?(keys) do
+    assert_list(keys)
+    keys |> Enum.map(&key?/1) |> Lens.multiple
+  end
+
+  @doc ~S"""
+  Returns a lens that points to all values of a keyword list.
+
+
+      iex> kw = [a: 1, b: 2, a: 3]
+      iex> lens = Lens.Keyword.values
+      iex> Deeply.get_all(kw, lens)
+      [1, 2, 3]
+      iex> Deeply.update(kw, lens, & &1 * 1111)
+      [a: 1111, b: 2222, a: 3333]
+  """
+  @spec values :: Lens2.lens
+  defmaker values do
+    Lens.all |> Lens.at(1)
+  end
 end
--- test/lenses/keyword_test.exs
+++ test/lenses/keyword_test.exs
@@ -1,6 +1,13 @@
 defmodule Lens2.Lenses.KeywordTest do
   use Lens2.Case, async: true
+  alias Lens2.Helpers.AssertionError
 
   doctest Lens2.Lenses.Keyword
+
+  test "atom assertion" do
+    assert_raise AssertionError, "key?/1 takes an atom as its argument.", fn ->
+      Lens.Keyword.key?(3)
+    end
+  end
 
 end
